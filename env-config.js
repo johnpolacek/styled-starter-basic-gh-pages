210653ac2800e3e702a95cb099b2ef5788d2f007
johnpolacek <johnpolacek@gmail.com>
2018-03-12 15:18:02 -0500

    update config env var

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
-console.log('process.env.BACKEND_URL', process.env.ENV_GH_PAGES ? '/styled-starter/' : '');
+console.log('process.env.BACKEND_URL', process.env.ENV_GH_PAGES ? '/styled-starter-basic-gh-pages/' : '');
 
 module.exports = {
-	'process.env.BACKEND_URL': process.env.ENV_GH_PAGES ? '/styled-starter/' : ''
+	'process.env.BACKEND_URL': process.env.ENV_GH_PAGES ? '/styled-starter-basic-gh-pages/' : ''
 }
